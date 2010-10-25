4a33ed5d9f1848f6c60217ab629e7254a0231979
Gerhard Lazu <gerhard@lazu.co.uk>
2010-10-25 14:07:34 +0100

    Fussing

--- definitions/screen_config.rb
+++ definitions/screen_config.rb
@@ -1,10 +1,10 @@
 define :screen_config, :owner => "root", :group => "root" do
   cookbook_file params[:name] do
+    cookbook "sysadmin"
     source "screenrc"
     owner params[:owner]
     group params[:group]
-    mode 0644
+    mode "0644"
     backup false
-    cookbook "sysadmin"
   end
 end
