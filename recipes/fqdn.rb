6d783b75288e7ecae82116df2c767a47d146533e
Gerhard Lazu <gerhard@lazu.co.uk>
2010-12-02 10:34:49 +0000

    Setting the FQDN via chef-solo Should also work for chef-server

--- recipes/fqdn.rb
+++ recipes/fqdn.rb
@@ -1,6 +1,6 @@
-hostname node.name
+hostname node[:name]
 
 host "Assigning our own FQDN" do
   search eval("/^#{node[:ipaddress]}/")
-  replace "#{node[:ipaddress]}\t#{node.name}"
+  replace "#{node[:ipaddress]}\t#{node[:name]}"
 end
