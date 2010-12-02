2e5947967b0cf30f840b8abf257a432bf583ebb3
Gerhard Lazu <gerhard@lazu.co.uk>
2010-12-02 11:53:32 +0000

    SSH keys are now hashes, not arrays
    Hardening SSH. Not reloading SSH just in case you need to edit your firewall...

--- recipes/default.rb
+++ recipes/default.rb
@@ -66,15 +66,16 @@ end
 if node.has_key? :ssh_keys
   ruby_block "Add my own SSH keys" do
     block do
-      file_write("/root/.ssh/authorized_keys", node[:ssh_keys].join("\n"))
+      ssh_keys = node[:ssh_keys].values.flatten.join("\n")
+      file_write("/root/.ssh/authorized_keys", ssh_keys)
     end
   end
 end
 
 ruby_block "Make SSH more secure" do
   block do
-    file_replace("/etc/ssh/sshd_config", /$.*Port\s\d+/, "Port #{node[:sysadmin][:sshd][:port]}")
-    file_replace("/etc/ssh/sshd_config", /$.*PasswordAuthentication\s\w+/, "PasswordAuthentication #{node[:sysadmin][:sshd][:password_authentication]}")
-    file_replace("/etc/ssh/sshd_config", /$.*PermitRootLogin\s\w+/, "PermitRootLogin #{node[:sysadmin][:sshd][:permit_root_login]}")
+    file_replace("/etc/ssh/sshd_config", /^.*Port\s\d+/, "Port #{node[:sysadmin][:sshd][:port]}")
+    file_replace("/etc/ssh/sshd_config", /^.*PasswordAuthentication\s\w+/, "PasswordAuthentication #{node[:sysadmin][:sshd][:password_authentication]}")
+    file_replace("/etc/ssh/sshd_config", /^.*PermitRootLogin\s\w+/, "PermitRootLogin #{node[:sysadmin][:sshd][:permit_root_login]}")
   end
 end
