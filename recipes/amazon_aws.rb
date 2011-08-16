445b457765d46ee86243b2085ae95a9cafd86bea
Gerhard Lazu <gerhard@lazu.co.uk>
2011-08-16 17:33:02 +0100

    Add all admin users to the default ubuntu user

--- recipes/amazon_aws.rb
+++ recipes/amazon_aws.rb
@@ -24,6 +24,20 @@ if File.exists? "/home/ubuntu"
     owner "ubuntu"
     group "ubuntu"
   end
+
+  node[:bootstrap][:users].each do |user, properties|
+    next unless properties[:admin]
+    properties[:keys].each do |key|
+      bash "adding #{user}'s SSH key to ubuntu user" do
+        code %{
+          if [ $(grep -c "#{key}" /home/ubuntu/.ssh/authorized_keys) -eq 0 ]; then
+            echo "#{key}" >> /home/ubuntu/.ssh/authorized_keys
+          fi
+          exit 0
+        }
+      end
+    end
+  end
 end
 
 directory "/mnt" do
