2202f3f54d9501d9d73d436187eb87c1c626e5f3
Gerhard Lazu <gerhard@lazu.co.uk>
2011-09-23 10:19:09 +0100

    Hostname becomes FQDN

--- definitions/hostname.rb
+++
@@ -1,7 +0,0 @@
-define :hostname do
-  execute "hostname #{params[:name]}"
-  # Ensure hostname will be the same after reboot
-  unless `cat /etc/hostname`.include? params[:name]
-    execute "echo '#{params[:name]}' > /etc/hostname"
-  end
-end
--- recipes/fqdn.rb
+++ recipes/fqdn.rb
@@ -19,9 +19,14 @@
 # limitations under the License.
 #
 
-hostname node[:bootstrap][:hostname]
+execute "Setting hostname to #{node[:fqdn]}" do
+  command %{
+    hostname #{node[:fqdn]}
+    echo #{node[:fqdn]} > /etc/hostname
+  }
+end
 
-host "Assigning #{node[:bootstrap][:hostname]} as the hostname" do
+host "Assigning #{node[:fqdn]} as the hostname" do
   search eval("/^#{node[:ipaddress]}/")
-  replace "#{node[:ipaddress]}\t#{node[:bootstrap][:hostname]}"
+  replace "#{node[:ipaddress]}\t#{node[:fqdn]}"
 end
