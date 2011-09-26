b9456b369049cc3899e8b6b52a18012ec6207e26
Gerhard Lazu <gerhard@lazu.co.uk>
2011-09-27 00:32:38 +0100

    screen_config is gone
    don't really care for those copyright notices
    This is OSS, no license with no strings attached
    
    Added first attribute, more to follow

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,7 @@ maintainer       "Gerhard Lazu"
 maintainer_email "gerhard@lazu.co.uk"
 license          "Apache 2.0"
 description      "Default bootstrap tasks"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0"
+version          "1.1.0"
 
 depends "apt"
 depends "ssh"
