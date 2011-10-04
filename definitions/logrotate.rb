e313f0731deffd4c2dcd5cb0e63b0f8791f34d92
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-04 09:57:45 +0100

    A more flexible logrotate (think apps with custom paths)

--- definitions/logrotate.rb
+++ definitions/logrotate.rb
@@ -1,18 +1,28 @@
-define :logrotate, :enable => true, :owner => "root", :group => "root", :period => "weekly", :keep => 4 do
-  if params[:enable]
-    template "/etc/logrotate.d/#{params[:name]}" do
-      cookbook "bootstrap"
-      source "logrotate.erb"
-      owner params[:owner]
-      group params[:group]
-      mode "0644"
-      variables(
-        :paths  => params[:paths],
-        :period => params[:period],
-        :keep   => params[:keep]
-      )
+define :logrotate,
+  :enable => true,
+  :owner => "root",
+  :group => "root",
+  :period => "weekly",
+  :keep => 4 do
+    app_name = params[:name]
+    logrotate_file = "/etc/logrotate.d/#{app_name}"
+
+    if params[:enable]
+      paths = params[:paths] || "/var/log/#{app_name}/*.log"
+
+      template logrotate_file do
+        cookbook "bootstrap"
+        source "logrotate.erb"
+        owner params[:owner]
+        group params[:group]
+        mode "0644"
+        variables(
+          :paths  => paths,
+          :period => params[:period],
+          :keep   => params[:keep]
+        )
+      end
+    else
+      execute "rm #{logrotate_file}"
     end
-  else
-    execute "rm /etc/logrotate.d/#{params[:name]}"
-  end
 end
