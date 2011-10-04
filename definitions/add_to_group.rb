4bc573b1385ed76d7f9bde504024569ceb17806b
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-04 09:58:06 +0100

    Using chef's group

--- definitions/add_to_group.rb
+++ definitions/add_to_group.rb
@@ -1,12 +1,14 @@
 define :add_to_groups, :groups => [] do
   username = params[:name]
 
-  params[:groups].each do |group|
-    bash "Adding #{username} to #{group} group" do
-      code "usermod -a -G #{group} #{username}"
+  params[:groups].each do |group_name|
+    group group_name
+
+    bash "Adding #{username} to #{group_name} group" do
+      code "usermod -a -G #{group_name} #{username}"
       only_if %{
         [ $(id #{username} 2>&1 | grep -c uid) -gt 0 ] && \
-        [ $(id #{username} | grep -c #{group}) -eq 0 ]
+        [ $(id #{username} | grep -c #{group_name}) -eq 0 ]
       }
     end
   end
