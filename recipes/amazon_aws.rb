37e68bae9789a60f40ee2fa31b970d04499a9de7
Gerhard Lazu <gerhard@lazu.co.uk>
2011-12-08 12:29:19 +0000

    Handling admin keys for EC2 ubuntu user

--- recipes/amazon_aws.rb
+++ recipes/amazon_aws.rb
@@ -1,16 +1,17 @@
 if File.exists? "/home/ubuntu"
-  node[:bootstrap][:users].each do |user, properties|
-    next unless properties[:admin]
-    properties[:keys].each do |key|
-      bash "adding #{user}'s SSH key to ubuntu user" do
-        code %{
-          if [ $(grep -c "#{key}" /home/ubuntu/.ssh/authorized_keys) -eq 0 ]; then
-            echo "#{key}" >> /home/ubuntu/.ssh/authorized_keys
-          fi
-          exit 0
-        }
-      end
+
+  # All users that are admins
+  #
+  admin_keys = node[:system_users].inject([]) { |result, (user, user_properties)|
+    if user_properties[:groups].include?("admin")
+      result << user[:keys]
     end
+    result
+  }.flatten
+
+  ssh_authorized_keys "ubuntu" do
+    ssh_keys admin_keys
+    home "/home/ubuntu"
   end
 end
 
