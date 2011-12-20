9b090b150f71740bb2f8c86d4db11da68b54ea39
Gerhard Lazu <gerhard@lazu.co.uk>
2011-12-20 14:52:10 +0000

    FQDN improvements

--- recipes/fqdn.rb
+++ recipes/fqdn.rb
@@ -1,11 +1,13 @@
-execute "Setting hostname to #{node[:fqdn]}" do
-  command %{
-    hostname #{node[:fqdn]}
-    echo #{node[:fqdn]} > /etc/hostname
+bash "Setting hostname to #{node.fqdn}" do
+  code %{
+    if [ $HOSTNAME != #{node.fqdn} ]; then
+      echo #{node.fqdn} > /etc/hostname
+      hostname -F /etc/hostname
+    fi
   }
 end
 
-host "Assigning #{node[:fqdn]} as the hostname" do
-  search eval("/^#{node[:ipaddress]}/")
-  replace "#{node[:ipaddress]}\t#{node[:fqdn]}"
+host "Assigning #{node.fqdn} as the hostname" do
+  search eval("/^#{node.ipaddress}/")
+  replace "#{node.ipaddress}\t#{node.fqdn}"
 end
