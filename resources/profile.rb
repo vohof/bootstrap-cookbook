b9e3f283a6c67138045b70c8d85d6ed5c5f3c6b6
Gerhard Lazu <gerhard@lazu.co.uk>
2012-01-13 11:38:50 +0000

    Bootstrap::User only requires 2 attributes, multiple name_attributes were messing things up

--- resources/profile.rb
+++ resources/profile.rb
@@ -1,19 +1,11 @@
-# Figure out how to extend system_user resource
-actions :create, :disable, :delete
+actions :create, :delete
 
-attribute :name,             :kind_of => String,  :name_attribute => true
-attribute :password,         :kind_of => String,  :default => ''
-attribute :groups,           :kind_of => Array,   :default => []
-attribute :allows,           :kind_of => Array,   :default => []
-attribute :home_basepath,    :kind_of => String,  :default => '/home'
-attribute :home_group,       :kind_of => String
-attribute :home_permission,  :kind_of => String,  :default => "0755"
-attribute :shell,            :kind_of => String,  :default => "/bin/bash"
-attribute :ssh_keys,         :kind_of => Array,   :default => []
+attribute :filename,       :kind_of => String,  :name_attribute => true
+attribute :params,         :kind_of => Array,   :required => true
 
-attribute :user,      :kind_of => Class
-attribute :filename,  :kind_of => String,  :name_attribute => true
-attribute :params,    :kind_of => Array,   :required => true
+attribute :username,       :kind_of => String
+attribute :home_basepath,  :kind_of => String,  :default => '/home'
+attribute :user,           :kind_of => Class
 
 def initialize(*args)
   super
