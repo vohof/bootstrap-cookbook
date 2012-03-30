2f342fd8d807fb9fbe1659f77545f94627a3af37
Gerhard Lazu <gerhard@lazu.co.uk>
2012-03-30 13:22:31 +0100

    A generic apps recipe Ruby, nodejs, java or even python, anything goes

---
+++ recipes/apps.rb
@@ -0,0 +1,24 @@
+node[:apps].each do |app|
+  next unless app.active
+
+  users_with_deploy_privileges = node[:system_users].inject([]) { |result, (user, user_properties)|
+    if user_properties[:groups].include?("deploy")
+      result << user_properties[:keys]
+    end
+    result
+  }.flatten
+
+  bootstrap_system_user app.name do
+    name app.name
+    groups (%w[deploy] + app.groups)
+    ssh_keys users_with_deploy_privileges
+    profile app.profile
+  end
+
+  # test manually: logrotate -vf /etc/logrotate.d/[app_name]
+  logrotate app.name do
+    paths "/var/log/#{app.name}/*.log"
+    period (app.logrotate_period || "daily")
+    keep (app.logrotate_keep || 7)
+  end
+end
