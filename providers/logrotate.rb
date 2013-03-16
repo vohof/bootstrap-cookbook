d830197b27e00e5c4c355ca2da3bf39eedcb3286
Gerhard Lazu <gerhard@lazu.co.uk>
2013-03-16 15:27:31 +0000

    Remove alias for delete

--- providers/logrotate.rb
+++ providers/logrotate.rb
@@ -17,6 +17,10 @@ action :delete do
   execute "rm #{@@logrotate_file}"
 end
 
+action :remove do
+  delete
+end
+
 def load_current_resource
   @@logrotate_file = "/etc/logrotate.d/#{new_resource.service}"
   @@path = new_resource.path || "/var/log/#{new_resource.service}/*.log"
