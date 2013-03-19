d58bf8507694758686355fcbb5c57e15c98e2c75
Gerhard Lazu <gerhard@lazu.co.uk>
2013-03-19 17:10:14 +0000

    This gets rid of all that annoying repetition

--- recipes/users.rb
+++ recipes/users.rb
@@ -1,18 +1,7 @@
-node[:system_users].each do |username, properties|
+node[:system_users].each do |username, user|
   bootstrap_system_user username do
-    action            properties[:action]
-    allows            properties[:allows]
-    git               properties[:git]
-    groups            properties[:groups]
-    home_basepath     properties[:home_basepath]
-    home_group        properties[:home_group]
-    home_permission   properties[:home_permission]
-    known_hosts       properties[:known_hosts]
-    password          properties[:password]
-    profile           properties[:profile]
-    shell             properties[:shell]
-    ssh_keys          properties[:keys]
-    ssh_private_key   properties[:ssh_private_key]
-    ssh_public_key    properties[:ssh_public_key]
+    user.each do |attribute, value|
+      instance_variable_set("@#{attribute}", value)
+    end
   end
 end
