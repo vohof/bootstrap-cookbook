e47685a2c7853f9e6a3abca7d0b351bc0271df3e
Cyrus David <vohof@users.noreply.github.com>
2014-05-02 21:11:01 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "gerhard@lazu.co.uk"
 license          "Apache 2.0"
 description      "Default bootstrap tasks"
 version          "1.9.2"
-
+name "bootstrap"
 depends "apt" # https://github.com/gchef/apt-cookbook
 depends "ssh" # https://github.com/gchef/ssh-cookbook
